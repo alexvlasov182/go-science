d1749c8309c27a2e5ef5f42941b4ab09e8759d47
Александр Власов <drumlife182@gmail.com>
2021-12-30 22:42:49 +0200

    some changes

--- array/array/array.go
+++ array/array/array.go
@@ -7,7 +7,9 @@ func main() {
 
 	// инициализация ззначениями по умолчанию
 	var a1 [3]int // [0,0,0]
+	var a2 [3]int
 	fmt.Println("a1 short", a1)
 	fmt.Printf("a1 short %v\n", a1)
 	fmt.Printf("a1 full %#v\n", a1)
+	fmt.Printf("a2 full %#v\n", a2)
 }
